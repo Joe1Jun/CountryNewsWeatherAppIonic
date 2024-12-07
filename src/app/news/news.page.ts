d751279628b8dfb1312037302478f32b5dddf5a1
Joe1Jun <junker91joe@gmail.com>
2024-12-07 16:03:03 +0100

    Added basic functionality to return the news from the API. This is achieved with an API key. MyHttpService method get is used for this

--- src/app/news/news.page.ts
+++ src/app/news/news.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { HttpOptions } from '@capacitor/core';
+import { MyHttpServiceService } from '../services/my-http-service.service';
 
 @Component({
   selector: 'app-news',
@@ -12,9 +14,31 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
 })
 export class NewsPage implements OnInit {
 
-  constructor() { }
+ news : any [] = [];
+ // The news Url with the API key. Should add ENV file for this key later
+ 
+ options : HttpOptions = {
+  url : "https://newsdata.io/api/1/latest?apikey=pub_61622a995ddc5cb7fb915bb2f0a5afa140633"
+}
+  
+constructor(private mhs : MyHttpServiceService){}
 
-  ngOnInit() {
+  ngOnInit(): void {
+      this.getNews();
   }
+
+  //This asynchronous function will call the get function from the myHttpService
+  async getNews(){
+     // Stores the response from the API in a varaible called response  
+    const response = await this.mhs.get(this.options);
+    // Log the data to the console to inspect the attributes
+    console.log(response.data.results);
+
+
+  }
+  
+
+
+  
 
 }
