03e057e1815a277b65547867ce93a66d06bf494b
Joe1Jun <junker91joe@gmail.com>
2024-12-07 13:10:27 +0100

    Added HTTP GET service using CapacitorHttpImplemented MyHttpServiceService with a method to handle HTTP GET requests. Utilized CapacitorHttp for making HTTP requests with specified HttpOptions that will be passed from various classes

--- src/app/services/my-http-service.service.ts
+++ src/app/services/my-http-service.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import { HttpOptions } from '@capacitor/core';
+import { CapacitorHttp } from '@capacitor/core';
 @Injectable({
   providedIn: 'root'
 })
 export class MyHttpServiceService {
 
   constructor() { }
+
+  async get (options : HttpOptions){
+    return await CapacitorHttp.get(options);
+  }
+
+
 }
