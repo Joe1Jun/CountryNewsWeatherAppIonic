9f799fc8f6877a568bf448eaf49afe8ecb31d1f2
Joe1Jun <junker91joe@gmail.com>
2024-12-07 13:04:49 +0100

    Created two variables in the CountriesPage class. Will first test the URL to get all country objects. Will later refine this to add parameters to the route

--- src/app/countries/countries.page.ts
+++ src/app/countries/countries.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { HttpOptions } from '@capacitor/core';
 
 @Component({
   selector: 'app-countries',
@@ -11,10 +12,18 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class CountriesPage implements OnInit {
+  // countries will hole all the country objects retreived from the API.
+  // Therefore is intialised as an empty array that can hold any type of data.
+  countries : any [] = [];
+  option : HttpOptions = {
+    url : "https://restcountries.com/v3.1/all"
+  }
 
   constructor() { }
 
   ngOnInit() {
+     
+
   }
 
 }
