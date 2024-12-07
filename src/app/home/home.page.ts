ad0b1784b6c33de4744e0070a189248929d885c8
Joe1Jun <junker91joe@gmail.com>
2024-12-07 12:58:23 +0100

    Added a routerLink to the search button that will route to the countries page after the click event

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton } from '@ionic/angular/standalone';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonButton, IonInput, IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonButton, IonInput, IonHeader, IonToolbar, IonTitle, IonContent,RouterLink],
 })
 export class HomePage {
   constructor() {}
