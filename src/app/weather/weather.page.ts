5a1974eadd195ca529613e979f874a01b2396105
Joe1Jun <junker91joe@gmail.com>
2024-12-08 13:55:59 +0100

    Added a boolean variable in the WeatherPage Class. When combined with *ngIf in the component page it allows the API to send a response before the page tries to load the attributes associated with the weather object defined in the component page

--- src/app/weather/weather.page.ts
+++ src/app/weather/weather.page.ts
@@ -15,25 +15,40 @@ import { HttpOptions } from '@capacitor/core';
 export class WeatherPage implements OnInit {
   // weather is actually an object as it only returns one object for each request
   weather : any  = {};
-
+  // Indicates whether data is being fetched.
+  loading! :boolean 
+// This variable hold the url to the API that returns the weather data
   options : HttpOptions = {
     url : "http://api.weatherstack.com/current?access_key=08547bc95ccf226b8a72dcf4d47e5e94&query=New York"
   }
 
-  // http://api.weatherstack.com/current
-  // ? access_key = 08547bc95ccf226b8a72dcf4d47e5e94
-  // & query = New York
+
   constructor(private mhs : MyHttpServiceService) { }
 
   ngOnInit() {
+    // Call the getWeather method from ngOnInit as it is not possible to define an async method in ngOnInit
     this.getWeather()
   }
 
   async getWeather(){
+    // Set loading to true while data is being fetched.
+      this.loading = true;
+   // Surround the API request in a try catch in case of errors.   
+     try {
+      // The Api response will be held in the response variable returned from myHttpService
       const response = await this.mhs.get(this.options);
-      console.log(response.data);
+      // Assign the object variable this.weather to the response data
      this.weather = response.data;
+     // Console log to see full object in the console
      console.log(this.weather);
+     // This will catch an error and display it in the console.
+     } catch (error) {
+        console.log("Error retrieving weather data" , error);
+      
+     } finally{
+      // Set the loading once again to false once the try catch block has completed.
+         this.loading = false;
+     }
 
 
   }
