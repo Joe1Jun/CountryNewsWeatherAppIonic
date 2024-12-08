d71deea0989a2a8b2485bfa8a8aeb0eb24399431
Joe1Jun <junker91joe@gmail.com>
2024-12-08 13:03:09 +0100

    Successfully loaded weather data for the New York example but only on the second attempt to route to the page. The forst attempt throws an error as the items have not been retrived from the API yet to display the title.

--- src/app/weather/weather.page.ts
+++ src/app/weather/weather.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard } from '@ionic/angular/standalone';
 import { MyHttpServiceService } from '../services/my-http-service.service';
 import { HttpOptions } from '@capacitor/core';
 
@@ -10,11 +10,11 @@ import { HttpOptions } from '@capacitor/core';
   templateUrl: './weather.page.html',
   styleUrls: ['./weather.page.scss'],
   standalone: true,
-  imports: [IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class WeatherPage implements OnInit {
-
-  weather : any [] = [];
+  // weather is actually an object as it only returns one object for each request
+  weather : any  = {};
 
   options : HttpOptions = {
     url : "http://api.weatherstack.com/current?access_key=08547bc95ccf226b8a72dcf4d47e5e94&query=New York"
@@ -32,7 +32,8 @@ export class WeatherPage implements OnInit {
   async getWeather(){
       const response = await this.mhs.get(this.options);
       console.log(response.data);
-     
+     this.weather = response.data;
+     console.log(this.weather);
 
 
   }
