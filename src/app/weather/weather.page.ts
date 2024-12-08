c978f00d62270c00291951c65831458a3231f288
Joe1Jun <junker91joe@gmail.com>
2024-12-08 12:32:05 +0100

    Added basic functionality to the weather page to retrieve weather. Will refactor the code now for all pages to add parameters

--- src/app/weather/weather.page.ts
+++ src/app/weather/weather.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { MyHttpServiceService } from '../services/my-http-service.service';
+import { HttpOptions } from '@capacitor/core';
 
 @Component({
   selector: 'app-weather',
@@ -12,9 +14,27 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
 })
 export class WeatherPage implements OnInit {
 
-  constructor() { }
+  weather : any [] = [];
+
+  options : HttpOptions = {
+    url : "http://api.weatherstack.com/current? access_key = 08547bc95ccf226b8a72dcf4d47e5e94 "
+  }
+
+  // http://api.weatherstack.com/current
+  // ? access_key = 08547bc95ccf226b8a72dcf4d47e5e94
+  // & query = New York
+  constructor(private mhs : MyHttpServiceService) { }
 
   ngOnInit() {
+    this.getWeather()
+  }
+
+  async getWeather(){
+      const response = await this.mhs.get(this.options);
+      console.log(response.data);
+
+      
+
   }
 
 }
