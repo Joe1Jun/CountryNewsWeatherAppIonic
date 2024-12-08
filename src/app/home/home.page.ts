a126eb685fb8ab0707b78233662d02f547631667
Joe1Jun <junker91joe@gmail.com>
2024-12-08 14:03:48 +0100

    Added ngModel to a variable that will be defined when the user enters the country they are searching for. This searchTerm will be passed to the end of the route path

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton } from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonButton, IonInput, IonHeader, IonToolbar, IonTitle, IonContent,RouterLink],
+  imports: [IonButton, IonInput, IonHeader, IonToolbar, IonTitle, IonContent,RouterLink, FormsModule],
 })
 export class HomePage {
+  searchTerm! : string
   constructor() {}
 }
