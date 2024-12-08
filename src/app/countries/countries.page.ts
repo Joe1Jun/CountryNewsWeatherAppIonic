5a1b32b54086ae96e0fdffdbd97cfcf5990c7cc6
Joe1Jun <junker91joe@gmail.com>
2024-12-08 14:40:25 +0100

    Integrated route parameter for country search and refactor API methods. Retrieved the searchTerm parameter from the route using ActivatedRoute.Added getCountriesFromParams method to fetch countries based on the searchTerm from the API.Dynamically construct the API URL within local variables for better readability and maintainability. Update ngOnInit to handle conditional logic.If a searchTerm exists, call getCountriesFromParams. Otherwise, fetch all countries using getAllCountries.Enhance error handling with console logs for debugging. Prepare structure for future refinement, such as adding an option to retrieve all countries from the homepage via another button

--- src/app/countries/countries.page.ts
+++ src/app/countries/countries.page.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonCardHeader, IonCard,  IonCardTitle,  IonButton } from '@ionic/angular/standalone';
 import { HttpOptions } from '@capacitor/core';
 import { MyHttpServiceService } from '../services/my-http-service.service';
-import { Router, RouterLink } from '@angular/router';
+import { ActivatedRoute,  RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-countries',
@@ -14,24 +14,45 @@ import { Router, RouterLink } from '@angular/router';
   imports: [IonButton,  IonCardTitle, IonCard, IonCardHeader, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,RouterLink]
 })
 export class CountriesPage implements OnInit {
+  // This will store the searchTerm parameter in the route.
+  searchTerm! : string;
+
   // countries will hole all the country objects retreived from the API.
   // Therefore is intialised as an empty array that can hold any type of data.
   countries : any [] = [];
-  options : HttpOptions = {
-    url : "https://restcountries.com/v3.1/all"
-  }
+  
 
-  constructor(private mhs : MyHttpServiceService) { }
+  // Activated Route will be used to get the route parameter and assign it to the variable searchTerm
+  constructor(private mhs : MyHttpServiceService, private route : ActivatedRoute) { }
 
   ngOnInit() {
+    // Use activated route to get the parameter from the route.
+    this.route.paramMap.subscribe((params) => {
+       // Extract searchTerm from the route or default to an empty string
+      this.searchTerm = params.get('searchTerm') || '';
+      //If searchTerm is there call the method to get the countries from the route parameter
+      if(this.searchTerm){
+        this.getCountriesFromParams();
+        //If there is no parameter retrieve all countries
+        //**** REFINE THIS LATER AS AN OPTION FROM THE HOMEPAGE */
+      } else{
+        this.getAllCountries(); 
+      }
+    })
+
     //Cant implement the function in NgOnInit as it cant handle asynchronous functions
-    this.getAllCountries(); 
+   
     
   }
 // This function will return a list of all countries by making an HTTP GET request.
 // It uses the 'get' method from MyHttpServiceService to fetch data based on the provided options.
     async getAllCountries(){
-        const response = await this.mhs.get(this.options);
+      // Assign the url to a local variable
+       let options : HttpOptions = {
+        url : "https://restcountries.com/v3.1/all"
+      }
+      try {
+        const response = await this.mhs.get(options);
         //Will console log to see how best to access the objects returned from the api.
         // Will then populate the countries array with these objects.
         console.log(response.data);
@@ -39,6 +60,31 @@ export class CountriesPage implements OnInit {
         this.countries = response.data;
         //Console log the array variable to see if it has been populated.
         console.log(this.countries);
+
+
+        
+      } catch (error) {
+        console.log("Error retrieving countries", error);
+      }
+        
+  }
+
+  async getCountriesFromParams(){
+      // Dynamically set the API URL for the search after 
+      // Assign the url to a local variable
+     let options : HttpOptions = {
+      url: `https://restcountries.com/v3.1/name/${this.searchTerm}`,
+    };
+    try {
+      const response = await this.mhs.get(options);
+      this.countries = response.data;
+      console.log(this.countries);
+
+    } catch (error) {
+      console.log("Error retrieving countries", error);
+    }
+
+
   }
 
 }
