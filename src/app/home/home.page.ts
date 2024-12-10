d82fdb48094d542a3c7882064fc06689d0ad75e0
Joe1Jun <junker91joe@gmail.com>
2024-12-10 10:54:24 +0100

    Tried to add the header component to the homepage through the html selector but it didnt work

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton } from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HeaderPage } from '../shared/header/header.page';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonButton, IonInput, IonHeader, IonToolbar, IonTitle, IonContent,RouterLink, FormsModule],
+  imports: [IonButton, IonInput, IonHeader, IonToolbar, IonTitle, IonContent,RouterLink, FormsModule,HeaderPage],
 })
 export class HomePage {
   searchTerm! : string
