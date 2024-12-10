434d6ec54dadd2f454941542db1d2e5fb254edb8
Joe1Jun <junker91joe@gmail.com>
2024-12-10 11:42:48 +0100

    Added basic style to the setting page. Will use a ngFor in later to loop through the options and make the display better. Created a back arrow from ion icons to link back to the home page after and item is selected

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCheckbox, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCheckbox, IonButton, IonIcon } from '@ionic/angular/standalone';
 import { DataServiceService } from '../services/data-service.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
   standalone: true,
-  imports: [IonButton, IonCheckbox, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonIcon, IonButton, IonCheckbox, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,RouterLink]
 })
 export class SettingsPage implements OnInit {
    
