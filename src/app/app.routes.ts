95696f0a0ba3415ec94bf61e5e33dc8a59df8554
Joe1Jun <junker91joe@gmail.com>
2024-12-10 11:09:52 +0100

    Added a setting icon to the header from the ion icons library. Had to use the svg because the web component code didnt work

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -30,14 +30,7 @@ export const routes: Routes = [
     path: 'settings',
     loadComponent: () => import('./settings/settings.page').then( m => m.SettingsPage)
   },
-  {
-    path: 'header',
-    loadComponent: () => import('./header/header.page').then( m => m.HeaderPage)
-  },
-  {
-    path: 'footer',
-    loadComponent: () => import('./footer/footer.page').then( m => m.FooterPage)
-  },
+  
   {
     path: 'header',
     loadComponent: () => import('./shared/header/header.page').then( m => m.HeaderPage)
