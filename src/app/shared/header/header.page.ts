a79297a3afb644674d421fedb2f5405b1f7e5135
Joe1Jun <junker91joe@gmail.com>
2024-12-10 11:18:37 +0100

    Made the settings icon wheel the link to the setting page. Removed the setting button from the home page

--- src/app/shared/header/header.page.ts
+++ src/app/shared/header/header.page.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon } from '@ionic/angular/standalone';
+import {  IonHeader, IonTitle, IonToolbar, IonIcon } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.page.html',
   styleUrls: ['./header.page.scss'],
   standalone: true,
-  imports: [IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [ IonIcon, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,RouterLink]
 })
 export class HeaderPage implements OnInit {
 
