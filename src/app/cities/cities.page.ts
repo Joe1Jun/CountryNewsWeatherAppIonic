3bb9929b6c18fb473b4943b199b47df6274fe889
Joe1Jun <junker91joe@gmail.com>
2024-12-18 13:01:08 +0000

    Altered the cities page whihc will be the new full weather page combining all the feautures from all the other original template pages. Successfully console logged the top 3 cities for countries

--- src/app/cities/cities.page.ts
+++ src/app/cities/cities.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonButton, IonInput, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle } from '@ionic/angular/standalone';
 import { ActivatedRoute } from '@angular/router';
@@ -8,6 +8,7 @@ import { HttpOptions } from '@capacitor/core';
 import { RouterLink } from '@angular/router';
 import { Router } from '@angular/router';
 import { Header2Page } from '../shared/header2/header2.page';
+import { DataServiceService } from '../services/data-service.service';
 @Component({
   selector: 'app-cities',
   templateUrl: './cities.page.html',
@@ -23,16 +24,14 @@ export class CitiesPage implements OnInit {
   countryCode! : string ;
   cityLimit : number = 3;
   userLocationInput! : string;
-  //This will hold the value of the lat and long values of the user input city returned from the api call 
-  latitude! : number;
-  longitude! : number;
+  
   units! : string;
   
 
 
   apiKey : string = '8bad249b0b4bef6ad8a518b937c7d010'
  
-  constructor(private route : ActivatedRoute, private router : Router, private mhs : MyHttpServiceService) { }
+  constructor(private route : ActivatedRoute, private router : Router, private mhs : MyHttpServiceService, private mds : DataServiceService) { }
 
   ngOnInit() {
 
@@ -42,14 +41,27 @@ export class CitiesPage implements OnInit {
 
 
     })
-  this.getCities();
+  
     
   }
 // This method is called when the user clicks the button to submit the data
-  
-ionViewWillEnter(){
-   this.getWeatherLocations();
-}
+  ionViewWillEnter(){
+    this.setUpTemperature();
+
+    }
+
+    setUpTemperature(){
+      this.getTempType().then(() => {
+        this.getCities()
+    
+    
+      })
+    }
+    async getTempType(){
+      const tempType = await this.mds.getItem('temperatureType');
+      this.units = tempType;
+      console.log(this.units)
+   }
 
 async getUserInputLocationCoordinates(){
     //checks if the user has inputted any data
@@ -63,15 +75,12 @@ async getUserInputLocationCoordinates(){
 
     const response = await this.mhs.get(options);
     console.log(response.data);
-    this.latitude = response.data[0].lat
-    this.longitude = response.data[0].lon;
-    console.log(this.latitude)
-    console.log(this.longitude); 
+     
+    this.getWeatherLocations(response.data[0].lat, response.data[0].lon) 
+    console.log(response.data[0].lat)
+    console.log(response.data[0].lon); 
     
-     // Navigate to the weather page with coordinates 
-     // If the API has returned correct coordinated the Router will automatically navigate to the weather page
-     // passing the parameters
-     this.router.navigate(['/weather', this.userLocationInput, this.latitude, this.longitude]);
+     
     }
     
      catch (error) {
@@ -106,7 +115,7 @@ try {
    this.cities = response.data.data
 
    for(let i = 0 ; i < this.cities.length; i++){
-         
+      this.getWeatherLocations( this.cities[i].latitude , this.cities[i].longitude)   
 
    }
   
@@ -122,10 +131,14 @@ try {
 
  async getWeatherLocations(lat : number, long : number){
      
- let  options : HttOptions = {
+ let  options : HttpOptions = {
     url : `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=${this.units}&appid=${this.apiKey}`
   }
 
+   let location = this.mhs.get(options);
+
+   this.weatherLocations.push(location)
+   console.log(this.weatherLocations)
   }  
 
 
