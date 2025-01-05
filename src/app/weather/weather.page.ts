6c0a664f562e37b032efa785c219b34b5bf8ac55
Joe1Jun <junker91joe@gmail.com>
2025-01-05 19:09:05 +0100

    Updated weather.html and switched the news article description and title

--- src/app/weather/weather.page.ts
+++ src/app/weather/weather.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule, Location } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonButton, IonInput, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonText, IonIcon, IonSpinner } from '@ionic/angular/standalone';
+import { IonContent, IonButton, IonInput, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonText, IonIcon } from '@ionic/angular/standalone';
 import { ActivatedRoute } from '@angular/router';
 import { MyHttpServiceService } from '../services/my-http-service.service';
 import { HttpOptions } from '@capacitor/core';
@@ -15,15 +15,14 @@ import { ApiService } from '../services/api.service';
   templateUrl: './weather.page.html',
   styleUrls: ['./weather.page.scss'],
   standalone: true,
-  imports: [IonSpinner, IonIcon, IonText, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard, IonInput, IonButton, IonContent, CommonModule, FormsModule,RouterLink, Header2Page]
+  imports: [IonIcon, IonText, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard, IonInput, IonButton, IonContent, CommonModule, FormsModule,RouterLink, Header2Page]
 })
 
 
 
 export class WeatherPage implements OnInit {
+// All the instance variables to be used by the class and component
 
-  // All the instance variables to be used by the class and component
-   
   apiKey! : string;
   capital! : string;
   capitalWeather : any = null // Declares it as an object instead of an array
@@ -35,16 +34,15 @@ export class WeatherPage implements OnInit {
   storedWeatherLocations : any [] = []
   isError : boolean = false;
   message! : string;
-  isWeather : boolean = true;
+
   
- // Initilises several services and routers to be used by the class
+  //Initialises several services and routers to be used by the class
   constructor(private route : ActivatedRoute, private router : Router, private mhs : MyHttpServiceService, private mds : DataServiceService, private mApi : ApiService) { }
 
   ngOnInit() {
-    // This retrieves the apikey from the Apiservice
+    // This retrieves the apikey from the service
     this.apiKey = this.mApi.getWeatherAPI();
-
-    // The parameters passed from the countries page are stored in the instance variables
+  // The parameters passed from the countries page are stored in the instance variables
     this.route.paramMap.subscribe((params) => {
       this.country = params.get('country') || ''
       this.countryCode = params.get('countryCode') || ' '
@@ -54,6 +52,7 @@ export class WeatherPage implements OnInit {
   
     
   }
+// This method is called when the user clicks the button to submit the data
 
   ionViewWillEnter(){
     //Need to clear all the instance varaible arrays so that duplicates
@@ -65,12 +64,10 @@ export class WeatherPage implements OnInit {
     this.capitalWeather = null;
     this.isError = false
 
-    
     this.setUpTemperature();
    
     }
-
-    // This method retrieved the temperature type from the settings storage 
+ // This method retrieved the temperature type from the settings storage 
     // The methods here are chained using asyn await and a try catch clock to ensure one completed only after the other has finished
     
    async  setUpTemperature(){
@@ -89,32 +86,21 @@ export class WeatherPage implements OnInit {
       console.log(this.units)
    }
 
-   //This method uses the capital passed from the countries page to retrieve weather data for the capital
+    //This method uses the capital passed from the countries page to retrieve weather data for the capital
    // This method is called first because it sits above all other weather cards.
-
    async getCapitalWeather() {
-
-    
     try {
-      //The url is passed to the helper method to retrieve the weather data 
+       //The url is passed to the helper method to retrieve the weather data 
       const url =  `https://api.openweathermap.org/data/2.5/weather?q=${this.capital}&units=${this.units}&appid=${this.apiKey}`
       
-  
+      
       const response = await this.getWeatherDataFromService(url);
-     
-      
       // Sets the reponse data to the object variable capitalWeather.
-      this.capitalWeather = response.data; // Store the capital's weather data
-      //Checks if there is a 404 code sent back in the data
-      
-      if(this.capitalWeather.cod === '404'){
-        this.isWeather = false;
-        this.message = `No weather data for  ${this.country}`
-      }
+      this.capitalWeather = response.data; 
       console.log(this.capitalWeather);
      
     } catch (error) {
-     console.log("Error retrieving data " , error)
+      console.log("Error fetching capital weather data:", error);
     }
   }
 
@@ -134,12 +120,10 @@ export class WeatherPage implements OnInit {
 
 // This sets the error flag to true if the location doesnt exist in the country the user is searching in.
   noDataForLocation(){
-     
      this.isError = true;
      this.message = `Location "${this.userLocationInput}" does not exist in ${this.country}`
     
   }
-
 
   async getUserInputLocationCoordinates(){
     //checks if the user has inputted any data
@@ -152,20 +136,16 @@ export class WeatherPage implements OnInit {
 
      
   try {
-   // Uses helper method to get data
+
     const response = await this.getWeatherDataFromService(url);
     let location = response.data
-    //  // If the name in the data doesnt match the user input it is set to the user input
-    //  // This is to avoid situation where different APIs can return different 
-    //   if(location.name.toLowerCase() !== this.userLocationInput ){
-    //     location.name = this.userLocationInput;
-    //   }
+     
+      if(location.name.toLowerCase() !== this.userLocationInput ){
+        location.name = this.userLocationInput;
+      }
      // This adds the user location to the array
-
-
-     //Pushes the weather item to the weatherLocations array 
      this.weatherLocations.push(location)
-    // This passed the weather item parameters to the method to save the items in storage.
+    
      this.saveWeatherLocations(location.id, location.name);
      
 
@@ -190,7 +170,7 @@ export class WeatherPage implements OnInit {
 
   
 
-  //This method loads all the weather locations saved previously by the user from storage
+  //This method loads all the weather locations saved previously by the user
   async getFavouriteWeatherLocations(){
     
     // Get the locations saved by the user from the data service
@@ -200,7 +180,7 @@ export class WeatherPage implements OnInit {
    
     
      //Loop through the locations and pass the data to the getWeather function so that the latest weather for these
-     // stored locations can be retrieved from the API
+     // stored locations can be retrieved from the
       for(let i = 0 ;i <  this.storedWeatherLocations.length; i++){
 
         this.getStoredLocationsCurrentWeather(this.storedWeatherLocations[i].id, this.storedWeatherLocations[i].name);
@@ -211,7 +191,6 @@ export class WeatherPage implements OnInit {
 
 async getStoredLocationsCurrentWeather(id:  number, name : string){
      
-  // This url allows for the unque id to be passed from the items retrieved from the storage array to the api directly 
   const url = `https://api.openweathermap.org/data/2.5/weather?id=${id}&units=${this.units}&appid=${this.apiKey}`
 
   
@@ -245,22 +224,20 @@ async getStoredLocationsCurrentWeather(id:  number, name : string){
 
 
 }
-//This method saves the weather location added by the user automatically to storage
 
   saveWeatherLocations(id : number, name : string){
-    
+    //Should just save the parameters needed to make the API call from the favourites page
     
     // This object holds the values to be saved to storage
     const location = {
       id : id,
       name : name
     }
-    // Used the data service to save the items to the storage array
+
     this.mds.saveItemToArray("weather" + this.countryCode, location )
     
   }
 
-  //Removes the location from the array using the key of the unique id number of the 
   async removeLocation(id : number){
 
     console.log(id)
@@ -272,7 +249,7 @@ async getStoredLocationsCurrentWeather(id:  number, name : string){
        // Clear the existing weather locations array
        this.weatherLocations = [];
        console.log("Storage array : " + this.mds.getArray("weather" + this.countryCode))
-      // Fetch the updated weather data by calling the ionic life cycle again.
+      // Fetch the updated weather data
       await this.ionViewWillEnter();
     } catch (error) {
       console.log("Error removing item or updating weather locations", error);
